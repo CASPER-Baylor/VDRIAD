fd56e997b496bbcb32c86f3f4502bcd7fff795c2
JAMO2001 <85181632+JAMO2001@users.noreply.github.com>
2023-02-14 17:46:02 -0600

    Finished implementing load parameters func

--- src/vdriadLoadParameters.m
+++ src/vdriadLoadParameters.m
@@ -20,34 +20,34 @@ function vdriadLoadParameters(app)
         
 
         % ELECTRON
-        app.params.ELECTRON_TEMPERATURE    = 28815;
-        app.params.ELECTRON_CHARGE         = -1.60e-19;
+        app.params.ELECTRON_TEMPERATURE    = loadParam('ELECTRON_TEMPERATURE',fileParam);
+        app.params.ELECTRON_CHARGE         = loadParam('ELECTRON_CHARGE',fileParam);
         app.params.ELECTRON_DENSITY        = nan;
         app.params.ELECTRON_DEBYE          = nan;
         
-        app.params.SHEATH_HEIGHT           = 10.6e-3;
-        app.params.WAKE_CHARGE_PERCENT     = 0.3;
-        app.params.WAKE_LENGTH             = 45e-6;
-        app.params.CUTOFF_MULTIPLIER       = 6;
+        app.params.SHEATH_HEIGHT           = loadParam('SHEATH_HEIGHT',fileParam);
+        app.params.WAKE_CHARGE_PERCENT     = loadParam('WAKE_CHARGE_PERCENT',fileParam);
+        app.params.WAKE_LENGTH             = loadParam('WAKE_LENGTH',fileParam);
+        app.params.CUTOFF_MULTIPLIER       = loadParam('CUTOFF_MULTIPLIER',fileParam);
 
-        app.params.NUM_PARTICLES           = 100;
-        app.params.DUST_DENSITY            = 1.51 * 1e3;
-        app.params.DUST_DIAMETER_MEAN      = 8.89e-6;
-        app.params.DUST_RADIUS_MEAN
-        app.params.DUST_DIAMETER_STD       = 0.01e-6;
-        app.params.DUST_CHARGE_DENSITY_MEAN= 1000 * 1e6;
-        app.params.DUST_CHARGE_DENSITY_STD = 20 * 1e6;
+        app.params.NUM_PARTICLES           = loadParam('NUM_PARTICLES',fileParam);
+        app.params.DUST_DENSITY            = loadParam('DUST_DENSITY',fileParam);
+        app.params.DUST_DIAMETER_MEAN      = loadParam('DUST_DIAMETER_MEAN',fileParam);
+        app.params.DUST_RADIUS_MEAN        = nan;
+        app.params.DUST_DIAMETER_STD       = loadParam('DUST_DIAMETER_STD',fileParam);
+        app.params.DUST_CHARGE_DENSITY_MEAN= loadParam('DUST_CHARGE_DENSITY_MEAN',fileParam);
+        app.params.DUST_CHARGE_DENSITY_STD = loadParam('DUST_CHARGE_DENSITY_STD',fileParam);
         
         % CONSTANTS
-        app.params.COULOMB                 = 8.98e12;
-        app.params.BOLTZMANN               = 1.38e-23;
-        app.params.GRAVITY                 = 9.8;
-        app.params.PERMITTIVITY            = 8.85418782e-12;
+        app.params.COULOMB                 = loadParam('COULOMB',fileParam);
+        app.params.BOLTZMANN               = loadParam('BOLTZMANN',fileParam);
+        app.params.GRAVITY                 = loadParam('GRAVITY',fileParam);
+        app.params.PERMITTIVITY            = loadParam('PERMITTIVITY',fileParam);
 
-        app.params.CELL_CHARGE             = 1.6e2;
-        app.params.CELL_RADIUS             = 0.6e-2;
-        app.params.CELL_HEIGHT             = 1.2e-2;
-        app.params.E_0                     = 3350;
+        app.params.CELL_CHARGE             = loadParam('CELL_CHARGE',fileParam);
+        app.params.CELL_RADIUS             = loadParam('CELL_RADIUS',fileParam);
+        app.params.CELL_HEIGHT             = loadParam('CELL_HEIGHT',fileParam);
+        app.params.E_0                     = loadParam('E_0',fileParam);
 end
 
 function param = loadParam(name,filename)
