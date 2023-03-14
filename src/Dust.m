587d45dd043d18726831e17db5f2de82bbcdffa1
JAMO2001 <jorge_martinez3@baylor.edu>
2023-03-14 09:53:07 -0500

    Changes to dust class and updated data

--- src/Dust.m
+++ src/Dust.m
@@ -1,8 +1,8 @@
-classdef Dust < Handle
+classdef Dust < handle
     %DUST class to store the information of dust particles in the
     %simulation
     %   Detailed explanation goes here
-    properties (SetAccess = public)
+    properties (SetAccess=public)
         Mass 
         Diameter
         Radius
@@ -16,11 +16,11 @@ classdef Dust < Handle
         WakeNNZ
         WakeNNId
     end
-
-    methods
+    methods (Access=public)
         function obj = Dust(params)
         %Dust is the constructor for the Dust class. 
-        GenerateParticles(params);
+        obj.GenerateParticles(params);
+        %obj.MemCpy('HtoD','all');
         end
         function GenerateParticles(obj,params)
         %GenerateParticles creates the particles based on the properties
@@ -79,31 +79,86 @@ classdef Dust < Handle
             % GENERATE SIZE, MASS AND CHARGE
             obj.Diameter.host = (randn(params.NUM_PARTICLES,1,'single') * params.DUST_DIAMETER_STD + params.DUST_DIAMETER_MEAN);
             obj.Radius.host   = obj.Diameter.host / 2;
-            obj.Mass.host     = params.DUST_DENSITY * (4/3 * pi * radius.^3);
-            obj.Charge.host   = round(params.DUST_CHARGE_DENSITY_MEAN * diameter) * params.ELECTRON_CHARGE;
+            obj.Mass.host     = params.DUST_DENSITY * (4/3 * pi * obj.Radius.host.^3);
+            obj.Charge.host   = round(params.DUST_CHARGE_DENSITY_MEAN * obj.Diameter.host) * params.ELECTRON_CHARGE;
         
             % GENERATE ION WAKE
-            obj.WakeChargePercent          = params.WAKE_CHARGE_PERCENT * ones(params.NUM_PARTICLES,1,'single');
-            obj.WakeLength                 = params.WAKE_LENGTH * ones(params.NUM_PARTICLES,1,'single');
-            obj.WakeNNR                    = 1000 * params.ION_DEBYE * ones(params.NUM_PARTICLES,1,'single');
-            obj.WakeNNZ                    = 1000 * params.ION_DEBYE * ones(params.NUM_PARTICLES,1,'single');
-            obj.WakeNNId                   = -1 * ones(params.NUM_PARTICLES,1,'int32');
+            obj.WakeChargePercent.host          = params.WAKE_CHARGE_PERCENT * ones(params.NUM_PARTICLES,1,'single');
+            obj.WakeLength.host                 = params.WAKE_LENGTH * ones(params.NUM_PARTICLES,1,'single');
+            obj.WakeNNR.host                    = 1000 * params.ION_DEBYE * ones(params.NUM_PARTICLES,1,'single');
+            obj.WakeNNZ.host                    = 1000 * params.ION_DEBYE * ones(params.NUM_PARTICLES,1,'single');
+            obj.WakeNNId.host                   = -1 * ones(params.NUM_PARTICLES,1,'int32');
         
             % SAVE INITIAL CONDITIONS
-            obj.Position.Initial.x          = xVec;
-            obj.Position.Initial.y          = yVec;
-            obj.Position.Initial.z          = zVec;
-            obj.Velocity.Initial.x          = obj.Velocity.host.x;
-            obj.Velocity.Initial.y          = obj.Velocity.host.y;
-            obj.Velocity.Initial.z          = obj.Velocity.host.z;
-            obj.Acceleration.Initial.x      = obj.Acceleration.host.x;
-            obj.Acceleration.Initial.y      = obj.Acceleration.host.y;
-            obj.Acceleration.Initial.z      = obj.Acceleration.host.z;
+            obj.Position.initial.x          = xVec;
+            obj.Position.initial.y          = yVec;
+            obj.Position.initial.z          = zVec;
+            obj.Velocity.initial.x          = obj.Velocity.host.x;
+            obj.Velocity.initial.y          = obj.Velocity.host.y;
+            obj.Velocity.initial.z          = obj.Velocity.host.z;
+            obj.Acceleration.initial.x      = obj.Acceleration.host.x;
+            obj.Acceleration.initial.y      = obj.Acceleration.host.y;
+            obj.Acceleration.initial.z      = obj.Acceleration.host.z;
     end
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function MemCpy(obj,direction,varargin)
+            %MemCpy Moves data between the GPU and the host
+            copyAll = false;
+            if nargin > 3
+                error('Error: Invalid number of input arguments')
+            elseif nargin == 3
+                opt = varargin{1};
+                if strcmp(opt,'all')
+                    copyAll = true;
+                end
+            end
+        
+            if strcmp(direction,'HtoD')
+                % COPY POSITIONS
+                obj.Position.device.x = gpuArray(obj.Position.host.x);
+                obj.Position.device.y = gpuArray(obj.Position.host.y);
+                obj.Postioin.device.z = gpuArray(obj.Position.host.z);
+        
+                if copyAll
+                    % COPY VELOCITIES
+                    obj.Velocity.device.x = gpuArray(obj.Velocity.host.x);
+                    obj.Velocity.device.y = gpuArray(obj.Velocity.host.y);
+                    obj.Velocity.device.z = gpuArray(obj.Velocity.host.z);
+            
+                    % COPY ACCELERATIONS
+                    obj.Acceleration.device.x = gpuArray(obj.Acceleration.host.x);
+                    obj.Acceleration.device.y = gpuArray(obj.Acceleration.host.y);
+                    obj.Acceleration.device.z = gpuArray(obj.Acceleration.host.z);
+        
+                    % COPY DUST PARAMETERS
+                    obj.Diameter.device = gpuArray(obj.Diameter.Host);
+                    obj.Radius.device   = gpuArray(obj.Radius.Host);
+                    obj.Charge.device   = gpuArray(obj.Charge.Host);
+                    obj.Mass.device     = gpuArray(obj.Mass.Host);
+        
+                    obj.WakeChargePercent.device  = gpuArray(obj.WakeChargePercent.host);
+                    obj.WakeLength.device         = gpuArray(obj.WakeLength.host);
+                    obj.WakeNNR.device          = gpuArray(obj.WakeNNR.host);
+                    obj.WakeNNZ.device          = gpuArray(obj.WakeNNZ.host);
+                    obj.WakeNNId.device         = gpuArray(obj.WakeNNId.host);
+                end
+            elseif strcmp(direction,'DtoH')
+                % COPY POSITIONS
+                obj.Position.host.x = gather(obj.Position.device.x);
+                obj.Position.host.y = gather(obj.Position.device.y);
+                obj.Position.host.z = gather(obj.Position.device.z);
+                
+                if copyAll
+                    % COPY VELOCITIES
+                    obj.Velocity.host.x = gather(obj.Velocity.device.x);
+                    obj.Velocity.host.y = gather(obj.Velocity.device.y);
+                    obj.Velocity.host.z = gather(obj.Velocity.device.z);
+            
+                    % COPY ACCELERATIONS
+                    obj.Acceleration.host.x = gather(obj.Acceleration.device.x);
+                    obj.Acceleration.host.y = gather(obj.Acceleration.device.y);
+                    obj.Acceleration.host.z = gather(obj.Acceleration.device,z);
+                end
+            end
         end
     end
 end
