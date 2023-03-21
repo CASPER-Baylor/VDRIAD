2a7a8c8c2953b7d8ebb92637e9bc39c25ea561c1
JAMO2001 <jorge_martinez3@baylor.edu>
2023-03-21 12:06:09 -0500

    Deletede some unused files

--- scripts/LoTa_ne_PwrPrs.m
+++
@@ -1,9 +0,0 @@
-% File Name:        LoTa_ne_PwrPrs.m
-% Author:           Jorge A. Martinez-Ortiz
-% Due Date:         02.16.2023
-% Description:      Lo(ook-up)Ta(ble) ElectronDensity(Power,Pressure)
-%                   Matlab script to generate the lookup table of the
-%                   electron density as a function of the power and
-%                   pressure. The values are taken from nosenkos paper.
-
-T = readtable();
