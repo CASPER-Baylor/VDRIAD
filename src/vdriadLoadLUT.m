1b934846ef919fb19f8d863fcd8984223454b709
JAMO2001 <jorge_martinez3@baylor.edu>
2023-06-22 16:45:28 -0600

    Deleted obsolete function files

--- src/vdriadLoadLUT.m
+++
@@ -1,7 +0,0 @@
-function vdriadLoadLUT(app)
-%LoadLut Loads the lookup table data previously generated to be used as a
-%lookup table in the simulation
-    load('../data/LUTData.mat','LUTData');
-
-    app.LUTS.LUTDensity = vdriadLUT2D(LUTData);
-end
